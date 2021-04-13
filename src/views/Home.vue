d3c7662bb2df9975e4fb296fd96591c5f76d102d
Ben Prat <bvprat@gmail.com>
2021-04-12 18:28:46 -0600

    Add initial form structure

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <form v-on:submit.prevent="submit()">
+      <h1>Add Information</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      <div class="form-group">
+        <label>Color:</label>
+        <input type="text" class="form-control" v-model="color">
+      </div>
+      <div class="form-group">
+        <label>Name:</label> 
+        <input type="text" class="form-control" v-model="name">
+      </div>
+      <div class="form-group">
+        <label>Style:</label>
+        <input type="text" class="form-control" v-model="style">
+      </div>
+      <div class="form-group">
+        <label> Price: </label>
+        <input type="text" class="form-control" v-model="price">
+      </div>
+      <div class="form-group">
+        <label>Capacity:</label>
+        <input type="text" class="form-control" v-model="capacity">
+      </div>
+      <input type="submit" class="btn btn-primary" value="Save + Download">
+    </form>
+  </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import axios from "axios"
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Boat Information File Conversion",
+      color: "",
+      name: "",
+      style: "",
+      price: "",
+      capacity:"",
+      boats: []
+    };
+  },
+  created: function() {
+    this.boatsIndex()
+  },
+  methods: {
+    saveBoatInformation() {
+      axios.post("/api/").then(response => {
+        console.log(response, response.data)
+        this.boats = response.data;
+      })
+    }
+  },
+};
 </script>
+
+//color name style price capacity
